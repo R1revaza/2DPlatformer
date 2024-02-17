5315ca5037fdbf866b4be3f195b02ee2d97d19b9
ioane37 <feiqrishviliioane@gmail.com>
2024-02-17 20:51:21 +0400

    Created new script

---
+++ Assets/Scenes/MyScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("How are you");
+        transform.position = new Vector3(10f, 0f, 5f);
+    }
+}
